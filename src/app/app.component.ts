581e9065ce7d5debc7e10f19d2200f30ac5c7bc8
Edgar Albalate <dev.gardo@gmail.com>
2017-02-28 17:16:19 +0100

    Simple navigation ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { HeroService } from './hero.service';
-import { Hero } from './hero';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [HeroService]
 })
 
-export class AppComponent implements OnInit {
-  ngOnInit() : void {
-    this.getHeroes()
-  }
+export class AppComponent {
   title = 'Tour of Heroes';
-  heroes : Hero[];
-  selectedHero : Hero;
-  
-  constructor(private heroService: HeroService){}
-  
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-  
-  getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
 }
--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,11 +1,33 @@
-import { Component, Input } from '@angular/core';
+import 'rxjs/add/operator/switchMap';
+
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService } from './hero.service';
 import {Hero} from './hero';
 
 @Component({
+  //moduleId: module.id,
   selector: 'my-hero-detail',
-  templateUrl: './hero-detail.component.html'
+  templateUrl: './hero-detail.component.html',
 })
-export class HeroDetailComponent {
-  @Input()
-  hero: Hero;
+export class HeroDetailComponent implements OnInit{
+  ngOnInit(): void {
+    this.route.params
+      .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+      .subscribe(hero => this.hero = hero);
+  }
+
+  @Input() hero: Hero;
+
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ){}
+
+  goBack(): void {
+    this.location.back();
+  }
 }
